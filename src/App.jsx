c822f677b1dda434f3b3d6812ee277fb1cbd97f4
John Pinkerton <johnpinker@yahoo.com>
2019-09-22 14:45:51 -0500

    updated some naming

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ListOfLinks from './ListOfLinks';
+import Header from './Header';
+import Footer from './Footer';
+
+class App extends React.Component {
+
+
+  render() {
+    return <div className="App">
+        <Header title="React Links App"/>
+        <ListOfLinks />
+        <Footer footerText="Testing and Playing c/o JPinx"/>
+      </div>
+  }
+}
+
+export default App;
---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = (props) => {
+    return <footer className="App-footer">
+    {props.footerText}
+  </footer>
+}
+
+export default Footer;
